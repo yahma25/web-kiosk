cb72df435592510a1c472628431fb379f931951c
mhkim <yahma25@gmail.com>
2021-04-18 00:59:42 +0900

    #1 package 일관성 있게 수정

--- webkiosk/src/test/java/com/web/kiosk/WebKioskApplicationTests.java
+++ webkiosk/src/test/java/com/web/kiosk/WebKioskApplicationTests.java
@@ -1,10 +1,10 @@
-package com.webkiosk.webkiosk;
+package com.web.kiosk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WebkioskApplicationTests {
+class WebKioskApplicationTests {
 
 	@Test
 	void contextLoads() {
